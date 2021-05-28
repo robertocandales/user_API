2214cd18e03946c395ee8692129fcc51f9ddbb85
Roberto Candales <candales.roberto@gmail.com>
2021-05-27 21:05:57 -0300

    allowed host changed v2

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for miniproject project.
 
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
